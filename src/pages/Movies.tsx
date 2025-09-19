6c5096efaf03a4370af5923eae747f162d642062
Ariel Wijaya <arielkusuma17@gmail.com>
2025-09-19 16:44:55 +0700

    add testing pages Movies

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "@/components/common/SearchInput";
+
+const Movies = () => {
+  return (
+    <div className="w-full px-8 mb-12">
+      <SearchInput className="mx-auto" />
+    </div>
+  );
+};
+
+export default Movies;
