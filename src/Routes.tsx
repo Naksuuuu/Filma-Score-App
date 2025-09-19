d79905785cdc2574c40418e32c013559f29eabe5
Ariel Wijaya <arielkusuma17@gmail.com>
2025-09-19 16:45:31 +0700

    add routes configuration

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/layouts/Layout";
+import Movies from "./pages/Movies";
 
 export const Routes = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ export const Routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
       },
     ],
   },
